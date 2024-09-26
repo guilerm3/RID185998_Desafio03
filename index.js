69c86e543b6327a2b8670ad696c031fbc74ee4d8
Guilherme Lucas <127703012+guilerm3@users.noreply.github.com>
2024-09-25 23:23:52 -0300

    .

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 let tasks = [
-    {id: 1, description:'Comer almoço'}
+    {id: 1, description:'Implementar tela de listagem de tarefas', label:'frontend'},
+    {id: 2, description:'Criar endpoint para cadastro de tarefas', label:'backend'},
+    {id: 3, description:'Implementar protótipo da listagem de tarefas', label:'ux'},
+    {id: 4, description:'Comer almoço', label:'frontend'},
 ]
+
+window.onload = function (){
+    tasks.forEach((task)=>{
+        const list = document.getElementById('todo-list')
+        const li = document.createElement('li')
+        const button = document.createElement('button')
+        const liLabel = document.createElement('li')
+        const span = document.createElement('span')
+        
+        li.id = task.id
+        span.textContent = task.description
+        liLabel.textContent = task.label
+        button.textContent = 'Concluir'
+        list.appendChild(li)
+        li.appendChild(span)
+        li.appendChild(liLabel)
+        li.appendChild(button)
+        
+    })
+}
